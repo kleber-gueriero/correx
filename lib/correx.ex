2a66dd41e707c6c03cd34ca3678712edb192bb7a
Kleber Nascimento Gueriero <kleber.gueriero@gmail.com>
2022-01-13 12:05:33 -0300

    init repository

---
+++ lib/correx.ex
@@ -0,0 +1,18 @@
+defmodule Correx do
+  @moduledoc """
+  Documentation for `Correx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Correx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/correx_test.exs
@@ -0,0 +1,8 @@
+defmodule CorrexTest do
+  use ExUnit.Case
+  doctest Correx
+
+  test "greets the world" do
+    assert Correx.hello() == :world
+  end
+end
